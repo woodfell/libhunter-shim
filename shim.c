5cd10c127b532ac5c51afb29a8e020f653e2ade9
Matt Woodward <matthew.woodward@swift-nav.com>
2019-04-09 21:50:51 +1000

    depend on unrelated

--- shim.c
+++ shim.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <libhunter-message/message.h>
+#include <libhunter-unrelated/unrelated.h>
 
 void hunter_shim(const char *parent)
 {
   printf("hunter_shim v1.0.0 (%s)\n", parent ?: "(none)");
   hunter_message("hunter_shim v1.0.0");
+  hunter_unrelated("hunter_shum v1.0.0");
 }
